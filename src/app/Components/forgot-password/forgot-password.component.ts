b06877b2b61d19d6dcb4f476064973d0b794e1ba
Vishal Singh <VishalSingh1597>
2021-11-23 17:27:50 +0530

    [Vishal] Add : Forgot and Reset Password

--- src/app/Components/forgot-password/forgot-password.component.ts
+++ src/app/Components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { UserServiceService } from 'src/app/Services/user-service.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  ForgotPasswordForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private userService:UserServiceService, 
+    private snackBar:MatSnackBar,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    this.ForgotPasswordForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
   }
 
+  ForgotPassword(){
+    this.userService.ForgotPassword(this.ForgotPasswordForm.value)
+    .subscribe((result:any)=>{
+      this.snackBar.open(`${result.message}`, '', {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'left'
+      });
+      if(result.status == true){
+        const params = {
+          CustomerId: result.data.customerId,
+          otp:result.data.otp
+        }
+        localStorage.setItem('BookStoreForgot', JSON.stringify(params));
+        this.router.navigate(['/resetPassword']);
+      }
+    },error => {  
+      this.snackBar.open(`${error.error.message}`, '', {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'left'
+      });
+    })
+  }
 }
--- src/app/Components/reset-password/reset-password.component.ts
+++ src/app/Components/reset-password/reset-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { UserServiceService } from 'src/app/Services/user-service.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
-
-  constructor() { }
+  hide = true;
+  ResetPasswordForm!: FormGroup;
+  constructor(
+    private userService:UserServiceService, 
+    private snackBar:MatSnackBar,
+    private router:Router) { }
 
   ngOnInit(): void {
+    this.ResetPasswordForm = new FormGroup({
+      password: new FormControl('', [Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$')]),
+      confirm: new FormControl('', [Validators.required]),
+      otp: new FormControl('',[Validators.required])
+    });
   }
 
+  ResetPassword(){
+    var data = localStorage.getItem('BookStoreForgot');
+    console.log(data);
+    var generatedOtp = JSON.parse(data!).otp;
+    if(generatedOtp == this.ResetPasswordForm.value.otp){
+      this.userService.ResetPassword(data,this.ResetPasswordForm.value)
+      .subscribe((result:any)=>{
+      this.snackBar.open(`${result.message}`, '', {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'left'
+      });
+      if(result.status == true)
+      {
+        this.router.navigate(['/login']);
+      }
+    }, error =>{
+      this.snackBar.open(`${error.error.message}`, '', {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'left'
+      });
+    })
+    }
+    else{
+      this.snackBar.open('Otp is invalid', '', {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'left'
+      });
+    }
+    
+  }
 }
